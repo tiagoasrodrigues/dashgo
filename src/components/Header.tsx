23ca2d320eeaac55edabcd7ab7c1d65c5ca10c3a
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-01-11 10:27:10 -0300

    :wrench: finalizing Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -48,6 +48,32 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize='20' />
+      </Flex>
+      <Flex
+        align='center'
+        ml='auto'
+      >
+        <HStack
+          spacing='4'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Tiago Augusto</Text>
+            <Text color='gray.300' fontSize='small'>
+              tiagoasrodrigues@protonmail.com
+            </Text>
+          </Box>
+          <Avatar size='md' name='Tiago Augusto' src='https://github.com/tiagoasrodrigues.png' />
+        </Flex>
       </Flex>
     </Flex>
   );
