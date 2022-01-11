c960fc94107d29c2b156bb1b69bb3bc7210d4f42
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-01-11 17:51:22 -0300

    :wrench: page: Dashboard

--- src/components/Form/Sidebar.tsx
+++ src/components/Form/Sidebar.tsx
@@ -1,7 +1,13 @@
 import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from "react-icons/ri";
+import {
+  RiDashboardLine,
+  RiContactsLine,
+  RiInputMethodLine,
+  RiGitMergeLine
+} from "react-icons/ri";
 
 export function Sidebar() {
+
   return (
     <Box as='aside' w='64' mr='8'>
       <Stack spacing='12' align='flex-start'>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,64 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from 'next/dynamic'
+import { ApexOptions } from 'apexcharts';
 import { Sidebar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2022-01-11T00:00:00.000Z',
+      '2022-01-12T00:00:00.000Z',
+      '2022-01-13T00:00:00.000Z',
+      '2022-01-14T00:00:00.000Z',
+      '2022-01-15T00:00:00.000Z',
+      '2022-01-16T00:00:00.000Z',
+      '2022-01-17T00:00:00.000Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [
+  { name: 'series1', data: [25, 230, 29, 5, 75, 180, 45] }
+];
 
 export default function Dashboard() {
   return (
@@ -9,6 +67,27 @@ export default function Dashboard() {
 
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
+
+        <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start'>
+          <Box
+            p='8'
+            bg='gray.800'
+            borderRadius={8}
+            pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Inscritos da Semana</Text>
+            <Chart options={options} series={series} type='area' height={160} />
+          </Box>
+          <Box
+            p='8'
+            bg='gray.800'
+            borderRadius={8}
+            pb='4'
+          >
+            <Text fontSize='lg' mb='4'>Taxa de Abertura</Text>
+            <Chart options={options} series={series} type='area' height={160} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
