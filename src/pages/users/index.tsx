8e901268371c7f833ca8802df80c3348fd9fa38f
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-02-21 12:29:50 -0300

    :wrench: Cadastro responsivo

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,7 @@ export default function UserList() {
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['4', '8']}>
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>Usuários</Heading>
 
@@ -38,8 +38,8 @@ export default function UserList() {
                   < Checkbox colorScheme='pink' />
                 </Th>
                 <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-
+                {isWideVersion && <Th>Data de cadastro
+                </Th>}
               </Tr>
             </Thead>
             <Tbody>
@@ -53,8 +53,8 @@ export default function UserList() {
                     <Text fontSize='sm' color='gray.300'>tiagoasrodrigues@protonmail.com</Text>
                   </Box>
                 </Td>
-                {isWideVersion && <Td>13 de Janeiro, 2022</Td>}
-
+                {isWideVersion && <Td>13 de Janeiro, 2022
+                </Td>}
               </Tr>
               <Tr>
                 <Td px={["4", "4", "6"]}>
@@ -66,8 +66,8 @@ export default function UserList() {
                     <Text fontSize='sm' color='gray.300'>tiagoasrodrigues@protonmail.com</Text>
                   </Box>
                 </Td>
-                {isWideVersion && <Td>13 de Janeiro, 2022</Td>}
-
+                {isWideVersion && <Td>13 de Janeiro, 2022
+                </Td>}
               </Tr>
               <Tr>
                 <Td px={["4", "4", "6"]}>
@@ -79,8 +79,8 @@ export default function UserList() {
                     <Text fontSize='sm' color='gray.300'>tiagoasrodrigues@protonmail.com</Text>
                   </Box>
                 </Td>
-                {isWideVersion && <Td>13 de Janeiro, 2022</Td>}
-
+                {isWideVersion && <Td>13 de Janeiro, 2022
+                </Td>}
               </Tr>
             </Tbody>
           </Table>
