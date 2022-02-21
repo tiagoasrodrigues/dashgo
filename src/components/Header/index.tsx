916c69d22d20575e6db13a5b7517f7b51addcdc2
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-02-21 11:11:27 -0300

    :wrench: Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri';
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -6,6 +9,8 @@ import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -22,6 +27,19 @@ export function Header() {
       px='6'
       align='center'
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )
+      }
       <Logo />
       {isWideVersion && <SearchBox />}
       <Flex
@@ -31,6 +49,6 @@ export function Header() {
         <NotificationsNav />
         <Profile showProfileData={isWideVersion} />
       </Flex>
-    </Flex>
+    </Flex >
   );
 }
---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useContext, useEffect } from "react";
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+interface SidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
+  const disclosure = useDisclosure()
+  const router = useRouter()
+
+  useEffect(() => {
+    disclosure.onClose()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.asPath])
+
+  /* useEffect(() => {
+    disclosure.onClose()
+  }, [disclosure, router.asPath]) */
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  )
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext)
