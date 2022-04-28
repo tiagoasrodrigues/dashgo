ea6e3402b9b2cd625d85b197de41725cd472c522
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-04-28 10:42:37 -0300

    :wrench: Formulários no React

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,14 @@
+import { useRef, useState } from "react";
 import { Flex, Icon, Input } from "@chakra-ui/react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+  // const [search, setSearch] = useState('')
+
+  const searchInputRef = useRef<HTMLInputElement>(null)
+
+  // searchInputRef.current.focus()
+
   return (
     <Flex
       as='label'
@@ -23,6 +30,7 @@ export function SearchBox() {
         mr='4'
         placeholder='Buscar na plataforma'
         _placeholder={{ color: 'gray.400' }}
+        ref={searchInputRef}
       />
       <Icon as={RiSearchLine} fontSize='20' />
     </Flex>
